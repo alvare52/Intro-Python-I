6fa4ff885f7cef6e23b4d011d389cd163f63e0ad
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-06-01 12:47:35 -0700

    03 done

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,28 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+for arg in sys.argv:
+    print(arg) # is this right? What are command line arguments exactly?
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print(sys.platform) # darwing
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(sys.version) # 3.7.7
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print(os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print(os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
